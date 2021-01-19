dbf9c044ac053bbf3cf26077cf96b5fd72f4b172
Oscar Bertrand <oscarb1989@yahoo.co.uk>
2021-01-19 14:43:28 +0000

    Create form erb file

--- app.rb
+++ app.rb
@@ -1,14 +1,13 @@
 require 'sinatra'
 set :session_secret, 'super secret'
-
-  # get '/' do
-  #   "Hello World"
-  # end
-
-  # get '/secret' do
-  # "Salar and Oscar are cool"
-  # end
   
-  get '/cat' do
+  get '/random-cat' do
+    @name = ["Amigo", "Oscar", "Viking"].sample
+    erb(:index)
+  end
+
+  get '/named-cat' do
+    p params
+    @name = params[:name]
     erb(:index)
   end
